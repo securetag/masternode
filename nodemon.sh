acc16f3951be798d0c38bdb1a93f11dd6050f79d
SecureTag <40545074+securetag@users.noreply.github.com>
2019-07-13 13:17:59 -0400

    Create nodemon.sh

---
+++ nodemon.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# nodemon 2.0 - SecureTag Masternode Monitoring
+
+#Processing command line params
+if [ -z $1 ]; then dly=5; else dly=$1; fi   # Default refresh time is 5 sec
+
+datadir="$HOME/.securetag$2"   # Default datadir is /root/.zeon
+
+# Install jq if it's not present
+dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
+
+echo '\n\nPress Ctrl-C to Exit...'
+
+
+#It is a one-liner script for now
+watch -ptn $dly "echo '===========================================================================
+Outbound connections to other SecureTag nodes [SecureTag datadir: $datadir]
+===========================================================================
+Node IP               Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
+Address               (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
+==========================================================================='
+sudo securetag-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
+\(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
+\((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' &&
+echo '==========================================================================='
+uptime
+echo '==========================================================================='
+echo 'Masternode Status: \n# securetag-cli masternode status' && sudo securetag-cli -datadir=$datadir masternode status
+echo '==========================================================================='
+echo 'Sync Status: \n# securetag-cli mnsync status' && sudo securetag-cli -datadir=$datadir mnsync status
+echo '==========================================================================='
+echo 'Masternode Information: \n# securetag-cli getinfo' && sudo securetag-cli -datadir=$datadir getinfo
+echo '==========================================================================='
+echo 'Usage: nodemon.sh [refresh delay] [datadir index]'
+echo 'Example: nodemon.sh 10 22 will run every 10 seconds and query securetagd in /$USER/.securetag22'
+echo '\n\nPress Ctrl-C to Exit...'"
